ff1c4e723ecb7a607c343546283864f89b8d1e27
luatil <luasantilli@gmail.com>
2020-08-18 19:40:44 -0300

    Correct string compare function
    This commit changes the body of the string_compare function.
    Before it was returning the index of the first character
    that was different between the strings;
    
    Signed-off-by: luatil <luasantilli@gmail.com>

--- main.c
+++ main.c
@@ -82,7 +82,7 @@ void getwords(char s1[], char s2[], char so[]) {
 
 int string_compare(char str1[], char str2[]) {
     int i = 0;
-    while(str1[i] != '\0') {
+    while(str1[i] != '\0' || str2[i] != '\0') {
         if(str1[i] != str2[i]) return 0;
         i++;
     }
